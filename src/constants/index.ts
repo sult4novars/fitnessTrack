acfef594c6bcc2e6328275ec4d3d9ceea9cbe8eb
Arsen <sultanov.arsen@luckymonkeys.games>
2023-03-05 17:54:38 +0300

    поправил запрос

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,3 +7,5 @@ export * from './utils';
 export * from './limits';
 
 export const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.mouep.mongodb.net/?retryWrites=true&w=majority`;
+
+export const DOMAIN = __prod__ ? process.env.NEXT_PUBLIC_DOMAIN : 'http://localhost:3000';
